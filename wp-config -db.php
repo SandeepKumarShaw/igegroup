687f45b82856c59e66227d555bd58cea8f892f86
sandeeptier5 <sandy.shaw20@gmail.com>
2017-06-16 16:53:35 +0530

    db configure

--- wp-config -db.php
+++ wp-config -db.php
@@ -22,13 +22,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'u408162910_igegr');
+define('DB_NAME', 'igegroup');
 
 /** MySQL database username */
-define('DB_USER', 'u408162910_igegr');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Igegroup@321');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
